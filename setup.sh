43040c051927d7889ed9148cbd8026d5b9d40e01
whs <hswongac@gmail.com>
2022-07-30 21:09:14 +0800

    Add qemu support

--- setup.sh
+++ setup.sh
@@ -5,4 +5,10 @@ apt-install openssh-server
 echo "packer ALL=(ALL) NOPASSWD: ALL" >> /target/etc/sudoers
 
 mkdir -p /target/home/packer/.ssh/
-printf '%b' "$(echo $PACKER_AUTHORIZED_KEY | sed 's/+/ /g;s/%/\\x/g')" > /target/home/packer/.ssh/authorized_keys
+
+if [ -n "$PACKER_AUTHORIZED_KEY" ]; then
+  printf '%b' "$(echo $PACKER_AUTHORIZED_KEY | sed 's/+/ /g;s/%/\\x/g')" > /target/home/packer/.ssh/authorized_keys
+elif [ -n "$PACKER_AUTHORIZED_KEY_FILE" ]; then
+  preseed_fetch $PACKER_AUTHORIZED_KEY_FILE /tmp/packer.pub
+  mv /tmp/packer.pub /target/home/packer/.ssh/authorized_keys
+fi
