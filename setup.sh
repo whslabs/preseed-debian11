1c02c07e8e47437e111e84aed2ad15c014b277c1
whs <hswongac@gmail.com>
2022-08-01 09:50:36 +0800

    Rename packer authorized key to ssh public key

--- setup.sh
+++ setup.sh
@@ -6,9 +6,9 @@ echo "packer ALL=(ALL) NOPASSWD: ALL" >> /target/etc/sudoers
 
 mkdir -p /target/home/packer/.ssh/
 
-if [ -n "$PACKER_AUTHORIZED_KEY" ]; then
-  printf '%b' "$(echo $PACKER_AUTHORIZED_KEY | sed 's/+/ /g;s/%/\\x/g')" > /target/home/packer/.ssh/authorized_keys
-elif [ -n "$PACKER_AUTHORIZED_KEY_FILE" ]; then
-  preseed_fetch $PACKER_AUTHORIZED_KEY_FILE /tmp/packer.pub
+if [ -n "$SSH_PUBLIC_KEY" ]; then
+  printf '%b' "$(echo $SSH_PUBLIC_KEY | sed 's/+/ /g;s/%/\\x/g')" > /target/home/packer/.ssh/authorized_keys
+elif [ -n "$SSH_PUBLIC_KEY_FILE" ]; then
+  preseed_fetch $SSH_PUBLIC_KEY_FILE /tmp/packer.pub
   mv /tmp/packer.pub /target/home/packer/.ssh/authorized_keys
 fi
